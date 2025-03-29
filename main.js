8ddcd2c8100f113cd69e1a35347c692d11344e5f
nelideven <nnyomandhipa@outlook.com>
2025-03-29 22:26:50 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /*
-    WandererBot - A ready-to-use JS minecraft bot script, using mineflayer, which lets you deploy minecraft bots and control them, either manually (via browser, like classic.minecraft.net) or automatically (following nearby entities)
+    WandererBot - A ready-to-use JS Minecraft bot script, using mineflayer, which lets you deploy Minecraft bots and control them, either manually (via browser, like classic.minecraft.net) or automatically (following nearby entities)
     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
